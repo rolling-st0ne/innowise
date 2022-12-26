33609350c9886b790a6b482efad6c1cfa77f4190
Mark Skoromets <mark@192.168.100.4>
2022-12-26 15:50:27 +0300

    ex1 edit

--- ex1/ex1.cpp
+++ ex1/ex1.cpp
@@ -3,13 +3,17 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
 const int BEES = 2023;
 
+// COMPILE IT WITH C++17
+
 void print_pair_two_pointers(vector<int>& input)
 {
+    sort(input.begin(), input.end());
     int p1 = 0, p2 = input.size() - 1;
 
     while (p1 != p2)
@@ -28,6 +32,20 @@ void print_pair_two_pointers(vector<int>& input)
     return ;
 }
 
+void print_pair_unordered_map(unordered_map<int, int>& input)
+{
+    for (auto& it: input)
+    {
+        if (input.find(BEES - it.first) != input.end())
+        {
+            cout << BEES - it.first << ' ' << it.first << endl;
+            return ;
+        }
+    }
+    cout << "No appropriate pair." << endl;
+    return ;
+}
+
 int main()
 {
     try
@@ -35,17 +53,16 @@ int main()
         ifstream input_file("Input task1.txt");
         if (!input_file)
             throw invalid_argument("Wrong file name");
-        vector<int> input_vector;
+        unordered_map<int, int> input_arr;
         string line;
         int numb;
         while (getline(input_file, line))
         {
             stringstream(line) >> numb;
-            input_vector.push_back(numb);
+            input_arr.try_emplace(numb, 1);
         }
-
-        sort(input_vector.begin(), input_vector.end());
-        print_pair_two_pointers(input_vector); 
+        // print_pair_two_pointers(input_vector); 
+        print_pair_unordered_map(input_arr);
         input_file.close();
     }
     catch(exception const& e)
